c923cb08551618e7688981dcea499a6a94342a6a
Anatoliy-Iliev <vekok12@gmail.com>
2021-05-18 22:32:37 +0300

    hw11-3

---
+++ src/js/index.js
@@ -0,0 +1,74 @@
+const refs = {  
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+}
+
+class CountdownTimer{
+  constructor({targetDate, clockFace}){
+    // this.intervalId = null;
+    this.targetDate = targetDate,
+    this.clockFace = clockFace
+  }
+  start(){
+    const startTime = this.targetDate;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime -currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      updateClockFace(time)
+    },
+      1000)
+  }
+  pad(value){
+  return String(value).padStart(2, '0');
+}
+
+  getTimeComponents(time) {
+  /*
+  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+  */
+  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+  /*
+  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+  * остатка % и делим его на количество миллисекунд в одном часе
+  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+  */
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+
+  /*
+  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+  */
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+  /*
+  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+  * миллисекунд в одной секунде (1000)
+  */
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return {days, hours, mins, secs}
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 10, 2021'),
+  clockFace: updateClockFace
+
+  // selectorTimer: document.querySelector(this.selector)
+});
+
+function updateClockFace({days, hours, mins, secs}){
+  refs.days.textContent = `${days}`
+  refs.hours.textContent = `${hours}`
+  refs.mins.textContent = `${mins}`
+  refs.secs.textContent = `${secs}`
+}
+
+timer.start()
