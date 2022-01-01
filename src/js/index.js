836e6dfa7b6e117ec2a9d04eaecd3720d217ece4
Anatoliy Iliev <vekok12@gmail.com>
2022-01-01 12:53:30 +0200

    fix: targetDate

--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,67 @@
-class CountdownTimer{
-  constructor({targetDate, selector}){  
-    this.targetDate = targetDate,
-    this.selector = selector,      
-    this.start(),
-    this.refs = {  
-      days: document.querySelector('[data-value="days"]'),      
-      hours: document.querySelector('[data-value="hours"]'),  
-      mins: document.querySelector('[data-value="mins"]'),  
-      secs: document.querySelector('[data-value="secs"]'),  
-    }    
+class CountdownTimer {
+  constructor({ targetDate, selector }) {
+    (this.targetDate = targetDate),
+      (this.selector = selector),
+      this.start(),
+      (this.refs = {
+        days: document.querySelector('[data-value="days"]'),
+        hours: document.querySelector('[data-value="hours"]'),
+        mins: document.querySelector('[data-value="mins"]'),
+        secs: document.querySelector('[data-value="secs"]'),
+      });
   }
-  start(){
+  start() {
     const startTime = this.targetDate;
 
     const intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = startTime - currentTime;
       const time = this.getTimeComponents(deltaTime);
-      this.updateClockFace(time)
-    })
+      this.updateClockFace(time);
+    });
   }
-  pad(value){
-  return String(value).padStart(2, '0');
+  pad(value) {
+    return String(value).padStart(2, '0');
   }
 
   getTimeComponents(time) {
-  /*
-  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-  */
-  const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    /*
+     * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+     * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+     */
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 
-  /*
-  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-  * остатка % и делим его на количество миллисекунд в одном часе
-  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-  */
-  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    /*
+     * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+     * остатка % и делим его на количество миллисекунд в одном часе
+     * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+     */
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
 
-  /*
-  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-  */
-  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    /*
+     * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+     * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+     */
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
-  /*
-  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-  * миллисекунд в одной секунде (1000)
-  */
-  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    /*
+     * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+     * миллисекунд в одной секунде (1000)
+     */
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  return {days, hours, mins, secs}
+    return { days, hours, mins, secs };
   }
 
-  updateClockFace({days, hours, mins, secs}){
-    this.refs.days.textContent = `${days}`
-    this.refs.hours.textContent = `${hours}`
-    this.refs.mins.textContent = `${mins}`
-    this.refs.secs.textContent = `${secs}`
+  updateClockFace({ days, hours, mins, secs }) {
+    this.refs.days.textContent = `${days}`;
+    this.refs.hours.textContent = `${hours}`;
+    this.refs.mins.textContent = `${mins}`;
+    this.refs.secs.textContent = `${secs}`;
   }
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 10, 2021'),
+  targetDate: new Date('May 24, 2022'),
 });
